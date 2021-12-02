cd125467c267c9fcadb470805e688639b1d1da46
riveralights <riveralights@gmail.com>
2021-12-02 19:14:36 +0700

    Membuat Layouts Login Page

---
+++ resources/views/layouts/admin/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Login</title>
+  @include('includes.admin.style')
+
+ </head>
+
+<body>
+  <div id="app">
+    <section class="section">
+      @yield('content')
+    </section>
+  </div>
+
+  @include('includes.admin.script')
+</body>
+</html>
