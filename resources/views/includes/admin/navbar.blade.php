d2f0df77a7330e39ff5cdbc388af39da7a6f50d8
SetiyawanID <kangari.id@gmail.com>
2022-01-09 08:00:17 +0700

    Mengubah Avatar dan Navbar

--- resources/views/includes/admin/navbar.blade.php
+++ resources/views/includes/admin/navbar.blade.php
@@ -6,7 +6,7 @@
     </form>
     <ul class="navbar-nav navbar-right">
         <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-        <img alt="image" src="{{ asset('backend/assets/img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
+        <img alt="image" src="{{ asset('backend/assets/img/avatar/avatar-custom.jpg') }}" class="rounded-circle mr-1">
         <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div></a>
         <div class="dropdown-menu dropdown-menu-right">
             <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="dropdown-item has-icon text-danger">
