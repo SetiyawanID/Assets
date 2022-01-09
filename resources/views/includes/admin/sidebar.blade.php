b200ca858eeb1b8f0c770e4578428bd754350eb7
Ari Setiyawan <arisetiyawan@Aris-MacBook-Pro.local>
2022-01-09 22:55:14 +0700

    Edit Sidebar

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -25,7 +25,7 @@
                     <li class="menu-header">Dashboard</li>
                     <li class="{{ Request::is('admin') ? ' active' : '' }}"><a class="nav-link" href="{{ route('dashboard.index') }}"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
                     <li class="{{ Request::is('admin/my-asset*') ? ' active' : '' }}"><a class="nav-link" href="{{ route('myasset.index', Auth::user()->id) }}"><i class="fas fa-box"></i> <span>My Assets</span></a></li>
-                    <li class="{{ Request::is('admin/my-license*') ? ' active' : '' }}"><a class="nav-link" href="{{ route('mylicense.index', Auth::user()->id) }}"><i class="fas fa-id-badge"></i> <span>My Licenses</span></a></li>
+                    <li class="{{ Request::is('admin/my-license*') ? ' active' : '' }}"><a class="nav-link" href="{{ route('mylicense.index', Auth::user()->id) }}"><i class="fas fa-check-circle"></i> <span>My Licenses</span></a></li>
                     <li class="{{ Request::is('admin/transfer*') ? ' active' : '' }}"><a class="nav-link" href="{{ route('transfer.index') }}"><i class="fas fa-id-badge"></i> <span>Asset Transfer Requests</span></a></li>
                     <li class="menu-header">Download</li>
                     <li class="{{ Request::is('admin/download-request') ? ' active' : '' }}"><a class="nav-link" href="{{ route('download.request') }}"><i class="fas fa-file-download"></i> <span>Download Form Request Assets</span></a></li>
@@ -35,7 +35,7 @@
                 @if(Auth::user()->role == "1")
                     <li class="menu-header">Main-Menu</li>
                     <li class="{{ Request::is('admin/asset*') ? ' active' : '' }}"><a class="nav-link" href="{{ route('asset.index') }}"><i class="fas fa-boxes"></i> <span>Assets</span></a></li>
-                    <li class="{{ Request::is('admin/license*') ? ' active' : '' }}"><a class="nav-link" href="{{ route('license.index') }}"><i class="fas fa-id-badge"></i> <span>Licenses</span></a></li>
+                    <li class="{{ Request::is('admin/license*') ? ' active' : '' }}"><a class="nav-link" href="{{ route('license.index') }}"><i class="fas fa-check-circle"></i> <span>Licenses</span></a></li>
                     <li class="{{ Request::is('admin/maintenance*') ? ' active' : '' }}"><a class="nav-link" href="{{ route('maintenance.index') }}"><i class="fas fa-wrench"></i> <span>Assets on Maintenance</span></a></li>
                     <li class="{{ Request::is('admin/transfer*') ? ' active' : '' }}"><a class="nav-link" href="{{ route('transfer.index') }}"><i class="fas fa-id-badge"></i> <span>Asset Transfer Requests</span></a></li>
                 @endif
